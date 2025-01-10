784b8c0723a7273238b67b38a6ed0a6eb4d9eeec
AlexChmp <alexandre.champier@gmail.com>
2025-01-10 15:52:21 +0100

    create project

---
+++ BibiDAcier/BibiDAcier/BibiDAcierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibiDAcierApp.swift
+//  BibiDAcier
+//
+//  Created by neoxia on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BibiDAcierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
