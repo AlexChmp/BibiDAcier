d8ebf205e0a3518ca5f370b2af0025370915b062
CalvignacNeoxia <charles.calvignac@neoxia.com>
2025-01-22 11:17:15 +0100

    change val index Selected

--- BibiDAcier/BibiDAcier/MainTabView.swift
+++ BibiDAcier/BibiDAcier/MainTabView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct MainTabView: View {
-    @State private var selectedIndex: Int = 1
+    @State private var selectedIndex: Int = 0
     
     var body: some View {
         TabView(selection: $selectedIndex) {
